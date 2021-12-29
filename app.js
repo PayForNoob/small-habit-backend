8000e50cfd201807522163d41c63911e023f5e10
t0dd-kr <swjasontodd@gmail.com>
2021-12-29 16:40:17 +0900

    commit test

--- app.js
+++ app.js
@@ -23,8 +23,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(join(__dirname, 'public')));
 
-
-
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
